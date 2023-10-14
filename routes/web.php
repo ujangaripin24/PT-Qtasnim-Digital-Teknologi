4703d18508af4e4616370d48af2344203f36612d
ujangaripin24 <ujangarifin06@gmail.com>
2023-10-14 10:13:11 +0700

    [07] ready to testing

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
+Route::resource('/', BarangController::class);
 Route::resource('/barang', BarangController::class);
